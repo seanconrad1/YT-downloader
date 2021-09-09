283422515fe44eec6c3e46326ba382509f4df785
sean conrad <seanconrad123@gmail.com>
2021-09-08 23:36:09 -0400

    pushing up latest

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
+const path = require('path');
 
 const app = express();
 app.use(cors());
-app.listen(4000, () => {
-  console.log('Server Works !!! At port 4000');
+app.use(express.static('public'));
+
+app.get('/', () => {
+  path.resolve('public');
 });
 
 app.get('/download', (req, res) => {
@@ -14,4 +17,8 @@ app.get('/download', (req, res) => {
   res.header('Content-Disposition', 'attachment; filename="video.mp4"');
 
   ytdl(URL, { format: 'mp4', quality: 'highestaudio' }).pipe(res);
+});
+
+app.listen(4000, () => {
+  console.log('http://localhost:4000');
 });
